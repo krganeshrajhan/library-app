e9ddab3f48446979b6107c85a48e77961b0aaad0
ganeshrajhan <krganeshrajhan@gmail.com>
2024-10-14 23:09:52 +0800

    Return book

--- 03-frontend/react-library/src/layouts/HomePage/Carousel.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/Carousel.tsx
@@ -1,21 +1,16 @@
+import { ReturnBook } from "./ReturnBook";
+
 export const Carousel = () => {
     return (
-        <><div className='container md-5' style={{ height: 550 }}>
+        <div className='container md-5' style={{ height: 550 }}>
             <h3>Find your next "I stayed up too late reading" book.</h3>
-        <div id='carouselExampleControls' className='carousel carousel-dark slide mt-5 d-none d-lg-block' data-bs-interval='false'>
+            <div id='carouselExampleControls' className='carousel carousel-dark slide mt-5 d-none d-lg-block' data-bs-interval='false'>
                 <div className='carousel-inner'>
                     <div className='carousel-item active'>
                         <div className='row d-flex justify-content-center align-items-center'>
-                            <div className='col-xs-6 col-sm-6 col-md4 col-lg-3 mb-3'>
-                                <img 
-                                    src={require('../../Images/BooksImages/book-luv2code-1000.png')}
-                                    width='151'
-                                    height='233'
-                                    alt='book1' />
-                                <h6 className='mt-2'>Book</h6>
-                                <p>Library</p>
-                                <a className='btn main-color text-white'>Reserve</a>
-                            </div>
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
                         </div>
                     </div>
                     <button className='carousel-control-prev' type='button'
@@ -31,23 +26,13 @@ export const Carousel = () => {
                 </div>
                 <div className='d-lg-none mt-3'>
                     <div className='row d-flex justify-content-center align-items-center'>
-                        <div className='text-center'>
-                            <img src={require('../../Images/BooksImages/book-luv2code-1000.png')}
-                                width='151'
-                                height='233'
-                                alt='book' />
-                            <h6 className='mt-2'>
-                                <b>Book</b>
-                            </h6>
-                            <p>Library</p>
-                            <a className='btn main-color text-white'>Reserve</a>
-                        </div>
+                        <ReturnBook />
                     </div>
                 </div>
                 <div className='homepage-carousel-title mt-3'>
                     <a className='btn btn-outline-secondary btn-lg' href='#'>View More</a>
                 </div>
             </div>
-        </div></>
+        </div>
     );
 }
