611fe205b7121ea452bc9107148365fd08c227d2
ganeshrajhan <krganeshrajhan@gmail.com>
2024-10-19 13:08:16 +0800

    Enable APIs as Read Only

---
+++ 02-backend/spring-boot-library/src/main/java/spring/boot/library/springbootlibrary/config/MyDataRestConfig.java
@@ -0,0 +1,26 @@
+package spring.boot.library.springbootlibrary.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import spring.boot.library.springbootlibrary.entity.Book;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration configuration, CorsRegistry corsRegistry) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
+
+        // disable HTTP methods for Book: PUT, POST, DELETE and PATCH
+        configuration.getExposureConfiguration()
+                .forDomainType(Book.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    }
+
+}
