a097ff8fb05c2394d3ea4fd38943f964d93958d1
ganeshrajhan <krganeshrajhan@gmail.com>
2024-10-19 14:15:43 +0800

    UseEffect to load Apis

--- 03-frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
@@ -1,7 +1,54 @@
 import { ReturnBook } from "../components/ReturnBook";
+import { useEffect, useState } from "react";
+import BookModel from "../../../models/BookModel";
 
 
 export const Carousel = () => {
+
+    const [books, setBooks] = useState<BookModel[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const baseUrl : string = 'http://localhost:8081/api/books';
+
+            const url: string = `${baseUrl}?page=0&size=9`;
+
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const responseJson = await response.json();
+
+            const responseData = responseJson._embedded.books;
+
+            const loadedBooks: BookModel[] = [];
+
+            for (const key in responseData) {
+                loadedBooks.push({
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    author: responseData[key].author,
+                    description: responseData[key].description,
+                    copies: responseData[key].copies,
+                    copiesAvailable: responseData[key].copiesAvailable,
+                    category: responseData[key].category,
+                    img: responseData[key].img
+                });
+            }
+
+            setBooks(loadedBooks);
+            setIsLoading(false);
+        };
+        fetchBooks().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+    }, []);
+
     return (
         <div className='container md-5' style={{ height: 550 }}>
             <h3>Find your next "I stayed up too late reading" book.</h3>
