b4672e5a1dc9683001d925e7e89e59682fcd326d
ganeshrajhan <krganeshrajhan@gmail.com>
2024-10-22 17:44:38 +0800

    Find By title

--- 02-backend/spring-boot-library/src/main/java/spring/boot/library/springbootlibrary/dao/BookRepository.java
+++ 02-backend/spring-boot-library/src/main/java/spring/boot/library/springbootlibrary/dao/BookRepository.java
@@ -1,7 +1,11 @@
 package spring.boot.library.springbootlibrary.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 import spring.boot.library.springbootlibrary.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
