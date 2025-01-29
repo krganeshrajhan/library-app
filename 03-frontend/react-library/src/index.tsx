12b9a559b987e212f46cf2c5d53af8e54cf197a2
ganeshrajhan <krganeshrajhan@gmail.com>
2025-01-29 16:58:43 +0800

    React routing with Switch

--- 03-frontend/react-library/src/index.tsx
+++ 03-frontend/react-library/src/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App'; // Import the 'App' component
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
