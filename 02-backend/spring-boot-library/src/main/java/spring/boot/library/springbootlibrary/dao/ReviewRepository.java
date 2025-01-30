6b8932e3ec65ca8945c6949a0000a47a24810cc8
ganeshrajhan <krganeshrajhan@gmail.com>
2025-01-30 17:55:20 +0800

    Search by Book Id

--- 02-backend/spring-boot-library/src/main/java/spring/boot/library/springbootlibrary/dao/ReviewRepository.java
+++ 02-backend/spring-boot-library/src/main/java/spring/boot/library/springbootlibrary/dao/ReviewRepository.java
@@ -1,8 +1,13 @@
 package spring.boot.library.springbootlibrary.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 import spring.boot.library.springbootlibrary.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    
+
+    Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
+
 }
