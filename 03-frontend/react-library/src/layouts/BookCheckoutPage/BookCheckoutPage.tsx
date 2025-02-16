df2427acce77dd10e10d1330571a7c7f563394d0
ganeshrajhan <krganeshrajhan@gmail.com>
2025-02-16 09:17:21 +0800

    Add ischeckedout by user

--- 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -58,7 +58,25 @@ export const BookCheckoutPage = () => {
 
     useEffect(() => {
         const fetchUserCheckedOutBook = async () => {
+            if (authState && authState.isAuthenticated) {
+                const url = `http://localhost:8081/api/books/secure/ischeckedout/byuser?bookId=${bookId}`;
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                        'Content-Type': 'application/json'
+                    }
+                };
+                const bookCheckedOut = await fetch(url, requestOptions);
 
+                if (!bookCheckedOut.ok) {
+                    throw new Error('Something went wrong!');
+                }
+
+                const bookCheckedOutResponseJson = await bookCheckedOut.json();
+                setIsCheckedOut(bookCheckedOutResponseJson);
+            }
+            setIsLoadingBookCheckedOut(false);
         }
         fetchUserCheckedOutBook().catch((error: any) => {
             setIsLoadingBookCheckedOut(false);
